b6524a2354fb6f30fcb3da82980734a36b194074
m-maekakuchi <m.marikoromo@gmail.com>
2022-09-27 21:13:15 +0900

    選択肢のWarningを修正

--- app/utilities/Form.php
+++ app/utilities/Form.php
@@ -16,13 +16,13 @@
       array_push($options, '<option value="">選択▼</option>');
       for ($i = 0; $i < count($list); $i++) {
         if ($key == 'work' || $key == 'address'){
-          if ($list[$i]['id'] == $_SESSION[$key]) {
+          if (isset($_SESSION[$key]) && $list[$i]['id'] == $_SESSION[$key]) {
             array_push($options, "<option value='{$list[$i]['id']}' selected>{$list[$i][$key]}</option>");
           } else {
             array_push($options, "<option value='{$list[$i]['id']}'>{$list[$i][$key]}</option>");
           }
         } else {
-          if ($list[$i] == $_SESSION[$key]) {
+          if (isset($_SESSION[$key]) && $list[$i] == $_SESSION[$key]) {
             array_push($options, "<option value='{$list[$i]}' selected>{$list[$i]}</option>");
           } else {
             array_push($options, "<option value='{$list[$i]}'>{$list[$i]}</option>");
